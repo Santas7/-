502e3803f3d95871d3718452b1ba0a00b86d5f14
Santas7 <andreypanyshkin03@gmail.com>
2022-09-14 20:23:34 +0400

    создание доп.переменных + подключение BeautifulSoup для реализации парсера

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,4 +1,5 @@
 import os, requests
+from bs4 import BeautifulSoup as BS
 
 __URL__ = "https://yandex.ru/images/" # ссылка на страничку html
 
@@ -9,3 +10,10 @@ while(True): # цикл проверки на корректный ввод ст
         break
     else:
         print("\nBad(\n") # ввод неудачный(
+
+
+__REQUEST__GO__ = requests.get(__URL__ + "search?text=" + __INPUT__) # получение веб-страницы, после чего можно получить всю необходимую нам информацию от объекта
+__HTML__ = BS(__REQUEST__GO__.content, "html.parser") # html codes
+__DATA__ = [] # массив который будет хранить все теги <img>
+__FINDER__ = __HTML__.findAll("img") # нахождение всех тегов <img>
+
