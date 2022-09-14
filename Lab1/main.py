693258f780f9f3e259c24d325da8fabb41954056
Santas7 <andreypanyshkin03@gmail.com>
2022-09-14 20:16:10 +0400

    ввод строки по которой будут найдены изображения

--- Lab1/main.py
+++ Lab1/main.py
@@ -2,4 +2,10 @@ import os, requests
 
 __URL__ = "https://yandex.ru/images/" # ссылка на страничку html
 
-
+while(True): # цикл проверки на корректный ввод строки
+    __INPUT__ = input("Please enter the text requests: ") # ввод запроса в строку поиска
+    if(__INPUT__ != ""):
+        print("\nNice!\n") # ввод успешный
+        break
+    else:
+        print("\nBad(\n") # ввод неудачный(
