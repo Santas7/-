412ccbdcd1968fc8f73505a77a58b958b55190f6
Santas7 <andreypanyshkin03@gmail.com>
2022-09-23 10:42:51 +0400

    добавил цикл while в get_image_url() который проходит по всем page до 999 и парсит соответственно

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,19 +1,17 @@
 import os, shutil, requests
 from bs4 import BeautifulSoup as BS
 
-url = "https://yandex.ru/images/" # ссылка на страничку html
-
+URL = "https://yandex.ru/images/" # ссылка на страничку html
 
 
 def save_image(image_url, name, i):
     request_on_download_image = requests.get(f"https:{image_url}")
-    saver = open(f"dataset/{name}/{i:04d}.jpg")
+    saver = open(f"dataset/{name}/{i:04d}.jpg", "wb")
     saver.write(request_on_download_image.content)
     saver.close()
 
 def clear_folder(name):
     shutil.rmtree(name)
-
 
 def check_folder():
     try:
@@ -24,20 +22,26 @@ def check_folder():
 
 def get_images_url(name):
     i = 1
-    request_go = requests.get(f"{url}search?text={name}", headers={"User-Agent":"Mozilla/5.0"})
+    page = 0
+    request_go = requests.get(f"{URL}search?p={page}&text={name}&lr=51&rpt=image", headers={"User-Agent":"Mozilla/5.0"})
     html = BS(request_go.content, "html.parser")
     data = []
     finder = html.findAll("img")
     os.mkdir(f"dataset/{name}")
-    for event in finder:
-        image_url = event.get("src")
-        data.append([image_url])
-        if(image_url != ""):
-            save_image(image_url, name, i)
-            i += 1
+    while (True):
+        for event in finder:
+            image_url = event.get("src")
+            data.append([image_url])
+            if (i > 999):
+                page = 0
+                break
+            if (image_url != ""):
+                save_image(image_url, name, i)
+                i += 1
+        if (i > 999): break
+        page += 1
     print("Nice save images)")
     print(data)
-
 
 check_folder()
 get_images_url("rose")
