c5b3aae4c717f1533d2ca67fb0ed8dee3acf241e
Santas7 <andreypanyshkin03@gmail.com>
2022-09-24 14:19:57 +0400

    add docstring in the code

--- Lab1/main.py
+++ Lab1/main.py
@@ -5,6 +5,7 @@ URL = "https://yandex.ru/images/" # ссылка на страничку html
 
 
 def save_image(image_url, name, i):
+    """сохранение спарсенной картинки в определенную папку"""
     request_on_download_image = requests.get(f"https:{image_url}")
     saver = open(f"dataset/{name}/{i:04d}.jpg", "wb")
     saver.write(request_on_download_image.content)
@@ -12,6 +13,7 @@ def save_image(image_url, name, i):
 
 
 def check_folder():
+    """проверка на существование папки"""
     try:
         os.mkdir("dataset")
     except:
@@ -19,6 +21,15 @@ def check_folder():
         os.mkdir("dataset")
 
 def get_images_url(name):
+    """
+    VARIABLES: создаем необходимые переменные для работы
+    FOR: Цикл for нужен для пробежки по finder ( по спарсеному html-коду ).
+    IMAGE_URL: Далее в переменную image_url записываем ссылку которая содержится в теге img, в параметре src="...".
+    DATA: Затем в массив data с помощью метода append добавляем в конец значение переменной image_url.
+    После проверяем через условный оператор if строку на пустоту.... если не пустая, то сохраняем картинку в
+    соответствующую папку dataset/name/....jpg/. После цикла выводим сообщение в консоль для пользователя.
+    На этом работа функции закончена.
+    """
     i = 1
     page = 0
     request_go = requests.get(f"{URL}search?p={page}&text={name}&lr=51&rpt=image", headers={"User-Agent":"Mozilla/5.0"})
@@ -41,6 +52,11 @@ def get_images_url(name):
     print("Nice save images)")
     print(data)
 
+help(get_images_url.__doc__)
+help(save_image.__doc__)
+help(check_folder.__doc__)
+
 check_folder()
 get_images_url("rose")
 get_images_url("tulip")
+
