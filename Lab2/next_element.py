2d1f6e6e4f3aca59fe30df2dab1a201e3aa878d2
Santas7 <andreypanyshkin03@gmail.com>
2022-10-21 15:58:46 +0400

    создал функцию (без реализации) get_path_of_object_class() - данная функция получает объект и пойнт(точку или метку) и возращает следующий элемент

---
+++ Lab2/next_element.py
@@ -0,0 +1,13 @@
+import csv
+import main
+from main import Data
+
+
+def get_path_of_object_class(obj: object, pointer: str) -> str:
+    """
+        данная функция получает объект и пойнт(точку или метку) и возращает следующий элемент
+    """
+
+
+if __name__ == "__main__":
+    pass
