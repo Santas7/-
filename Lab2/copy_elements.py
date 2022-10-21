9a30a4abbc27dfef632ef687154f140fdc20e57d
Santas7 <andreypanyshkin03@gmail.com>
2022-10-21 15:29:06 +0400

    добавил новые функции make_dir() - проверка на сущ-ю директорию + создание новой директории, и teleportdir() - данная функция создает новую папку new_dataset и переносит туда каталог class_name со всеми ее под-каталогами так что имена новых под-каталогов начинаются с class_name.

---
+++ Lab2/copy_elements.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+
+def make_dir(obj: object) -> None:
+    """
+        проверка на сущ-ю директорию + создание новой директории
+    """
+
+
+def teleportdir(obj: object) -> None:
+    """
+        данная функция создает новую папку new_dataset и переносит туда каталог class_name со всеми ее под-каталогами
+        так что имена новых под-каталогов начинаются с class_name
+    """
+
+
+if __name__ == "__main__":
+    pass
