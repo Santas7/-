f6cbdd3e950c98b0b61484330f61e1e06ae45208
Santas7 <andreypanyshkin03@gmail.com>
2022-10-21 16:10:35 +0400

    написал базовую реализацию функции get_path_of_object_class() - данная функция получает объект и пойнт(точку или метку) и возращает следующий элемент

--- Lab2/next_element.py
+++ Lab2/next_element.py
@@ -7,7 +7,22 @@ def get_path_of_object_class(obj: object, pointer: str) -> str:
     """
         данная функция получает объект и пойнт(точку или метку) и возращает следующий элемент
     """
-
+    with open(f'{obj.dir_name}/annotation.csv', 'r', newline='') as file:
+        wr = csv.reader(file, delimiter=' ', quotechar='|')
+        count = 0 # счетчик
+        res = [] # результирующий список
+        tmp = []
+        text = "" # временная строка
+        for row in wr:
+            # tmp.append(row)
+            pass
+        for i in range(len(tmp)):
+            if i != 0:
+                # res.append( .. )
+                pass
+        for path in range(len(res)):
+            if res[path] == pointer:
+                pass
 
 if __name__ == "__main__":
     pass
