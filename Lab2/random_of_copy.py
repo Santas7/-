eecfc67c1c3940715aabac1562fe2ddc92ae40c9
Santas7 <andreypanyshkin03@gmail.com>
2022-10-21 15:47:07 +0400

    добавил необходимые модули и создал функцию(без реализации) create_copy_dataset_with_random_number() - данная функция копирует файлы класса и переименовывает их рандомно

---
+++ Lab2/random_of_copy.py
@@ -0,0 +1,14 @@
+import csv
+import random
+import os
+import shutil
+
+
+def create_copy_dataset_with_random_number(obj: object) -> None:
+    """
+        данная функция копирует файлы класса и переименовывает их рандомно
+    """
+
+
+if __name__ == "__main__":
+    pass
