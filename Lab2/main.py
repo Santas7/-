c7074e0d150dc2f02b50045ad33a977286dd2b50
Santas7 <andreypanyshkin03@gmail.com>
2022-10-21 15:03:25 +0400

    создал и добавил класс Data (Аннотация), сделал подобие конструктора с параметрами class_name (str) и dir_name (str) + сделал функции доступа геттеры и сеттеры.

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,4 +1,44 @@
 import os
 import csv
 
-#
+
+class Data:
+    class_name: str
+    dir_name: str  # название директории напр. dataset
+    n_data: str  #
+
+    def __init__(self, class_name: str, dir_name: str):
+        """
+            конструктор с параметром class_name и dir_name ( название папки напр. dataset )
+        """
+        self.class_name = class_name
+        self.dir_name = dir_name
+        self.n_data = []
+
+    def set_class_name(self, new_class_name: str) -> None:
+        """
+            изменение class_name
+        """
+        self.class_name = new_class_name
+
+    def get_class_name(self) -> str:
+        """
+            получение значения class_name
+        """
+        return self.class_name
+
+    def set_dir_name(self, new_dir_name: str) -> None:
+        """
+            изменение dir_name
+        """
+        self.dir_name = new_dir_name
+
+    def get_dir_name(self) -> str:
+        """
+            получение значения dir_name
+        """
+        return self.dir_name
+
+
+if __name__ == "__main__":
+    pass
