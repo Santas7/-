175c3e46d298d785dfd0d9032b8c8eb857c26d0f
Santas7 <andreypanyshkin03@gmail.com>
2022-10-21 15:34:45 +0400

    написал реализацию функции teleportdir - данная функция создает новую папку new_dataset и переносит туда каталог class_name со всеми ее под-каталогами так что имена новых под-каталогов начинаются с class_name

--- Lab2/copy_elements.py
+++ Lab2/copy_elements.py
@@ -20,7 +20,16 @@ def teleportdir(obj: object) -> None:
         данная функция создает новую папку new_dataset и переносит туда каталог class_name со всеми ее под-каталогами
         так что имена новых под-каталогов начинаются с class_name
     """
-
+    later_dir = obj.get_dir_name()
+    make_dir(obj)
+    for i in range(999):
+        os.rename(f'{later_dir}/{obj.class_name}/{i + 1:04d}.jpg',
+                  f'{later_dir}/{obj.class_name}/{obj.class_name}_{i + 1:04d}.jpg')
+        shutil.copy(os.path.expanduser(f'{later_dir}/{obj.class_name}/{obj.class_name}_{(i + 1):04d}.jpg'),
+                    obj.dir_name)
+        os.rename(f'{later_dir}/{obj.class_name}/{obj.class_name}_{i + 1:04d}.jpg',
+                  f'{later_dir}/{obj.class_name}/{i + 1:04d}.jpg')
+    obj.create_data_for_csv_file(1) # type 1
 
 if __name__ == "__main__":
     pass
