c0a9135695a69e291f51c7ca55248d6c11b5cbbd
Santas7 <andreypanyshkin03@gmail.com>
2022-11-04 11:37:38 +0400

    изменил ( упростил ) работу функции next_element

--- Lab2/next_element.py
+++ Lab2/next_element.py
@@ -1,44 +1,26 @@
 import csv
-import main
 from main import Data
 
 
-def get_path_of_object_class(obj: type(Data), pointer: str) -> str:
+def next_element(obj: type(Data), pointer: str) -> str:
     """
         данная функция получает объект и пойнт(точку или метку) и возращает следующий элемент
     """
-    with open(f'{obj.dir_name}/annotation.csv', 'r', newline='') as file:
+    with open('annotation.csv', 'r', newline='') as file:
         wr = csv.reader(file, delimiter=' ', quotechar='|')
-        count = 0 # счетчик
-        res = [] # результирующий список
-        tmp = []
-        text = "" # временная строка
+        i = 0  # счетчик
+        status = False
         for row in wr:
-            tmp.append(row[1])
-        for i in range(len(tmp)):
             if i != 0:
-                for l in range(len(tmp[i])): # пробежка по-символьно
-                    if tmp[i][l] == ",":  # запятая
-                        count = l
-                        while count + 1 < len(tmp[i]):
-                            count += 1
-                            if tmp[i][count] == ",": # если встретили запятую то выхожим из цикла
-                                break
-                            else:
-                                text += tmp[i][count]
-                        res.append(text)
-                        text = ""
-                        count = 0
-        for path in range(len(res)): # пробежка по результируещему списку
-            if res[path] == pointer: # если данный маркер (pointer) совпадает с res[path] выводим нект элемент
-                return "the next-->" + str(res[path + 2])
+                exist = pointer in row[1]
+                if status:
+                    return "next--> " + str(row[1].split(",")[1])
+                if exist:
+                    status = True
+            i += 1
+    return None
 
 if __name__ == "__main__":
-    obj = Data("rose", "dataset")
-    s1 = get_path_of_object_class(obj, "dataset/rose/0015.jpg")  # dataset/rose/0016.jpg
-    print(s1)
-
-    obj = Data("tulip", "dataset")
-    s2 = get_path_of_object_class(obj, "dataset/tulip/0443.jpg")  # dataset/tulip/0444.jpg
-    print(s2)
-
+    obj = Data("dataset")
+    next = next_element(obj, "dataset\\rose\\0751.jpg") # dataset\rose\0752.jpg
+    print(next)
