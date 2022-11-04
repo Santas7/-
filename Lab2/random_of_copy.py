92028e49be3ad101980a591f0b2d974674401d69
Santas7 <andreypanyshkin03@gmail.com>
2022-11-04 12:40:00 +0400

    изменил create_copy_dataset_with_random_number() c os.path.join( .. )

--- Lab2/random_of_copy.py
+++ Lab2/random_of_copy.py
@@ -2,31 +2,27 @@ import csv
 import random
 import os
 import shutil
-
 import copy_elements as ce
 import main
 from main import Data
 
 
-def create_copy_dataset_with_random_number(obj: type(Data)) -> None:
+def create_copy_dataset_with_random_number(obj: type(Data), path: str, class_name: str) -> None:
     """
-        данная функция копирует файлы класса и переименовывает их рандомно
+        данная функция копирует файлы класса и переименовывает их рандомно. В конце цикла добавляем в файл аннотация.
+        :later_dir: - предыдущая директория ( нужно сохранить )
     """
     later_dir = obj.dir_name
     ce.make_dir(obj)
 
     for i in range(1000):
         n = random.randint(0, 10000)
-        os.rename(f'{later_dir}/{obj.class_name}/{i + 1:04d}.jpg',
-                  f'{later_dir}/{obj.class_name}/{n:05d}.jpg')
-        shutil.copy(os.path.join(f'{later_dir}/{obj.class_name}/{n:05d}.jpg'), obj.dir_name)
-        os.rename(f'{later_dir}/{obj.class_name}/{n:05d}.jpg',
-                  f'{later_dir}/{obj.class_name}/{i + 1:04d}.jpg')
-        obj.list_numbers.append(n)
-
-    obj.create_data_for_csv_file(2) # type 2
-
+        os.rename(os.path.join(later_dir, class_name, f'{(i+1):04d}.jpg'),
+                  os.path.join(later_dir, class_name, f'{n:05d}.jpg'))
+        shutil.copy(os.path.join(later_dir, class_name, f'{n:05d}.jpg'), obj.dir_name)
+        os.rename(os.path.join(later_dir, class_name, f"{n:05d}.jpg"),
+                  os.path.join(later_dir, class_name, f'{(i+1):04d}.jpg'))
+        obj.add(os.path.join(path, obj.dir_name, class_name), class_name, f'{n:05d}.jpg')  # добавление строки в файл аннотация
 
 if __name__ == "__main__":
-    for element in range(len(main.CLASS_DEFAULT)):
-        create_copy_dataset_with_random_number(Data(main.CLASS_DEFAULT[element], "dataset"))
+    create_copy_dataset_with_random_number(Data("dataset"), "D:\Program Files\programmingLabs\Python\python-L-2-var-4\Lab2", main.CLASS_DEFAULT[0])
