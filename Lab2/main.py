9bdab608b095976fbb0552b4c64f6d62f53e5b21
Santas7 <andreypanyshkin03@gmail.com>
2022-11-04 12:20:29 +0400

    изменил основной файл main.py: убрал много бесполезных методов класса и оставил только один (add - добавляет строку в файл аннотация и создает файл если его нет)

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,117 +1,35 @@
 import os
 import csv
-import logging
-import pandas as pd
 
 CLASS_DEFAULT = ["rose", "tulip"]  # базовые названия
 
 
 class Data:
-    def __init__(self, class_name: str, dir_name: str):
+    def __init__(self, dir_name: str) -> None:
         """
-            конструктор с параметром class_name и dir_name ( название папки напр. dataset )
-            :list_numbers: - список который в дальнейшем будет хранить в себе все рандомно сгенирируемые числа ( пункт 3 )
+            :number_lines: - кол-во строк в аннотации
+            :viewed_files: - кол-во просмотренных файлов
+            :dir_name: - название директории ( "dataset" )
         """
-        self.class_name = class_name
+        self.number_lines = 0
+        self.viewed_files = 1
         self.dir_name = dir_name
-        self.list_numbers = []
 
-    # 1 пункт л/р
-    def absolute_or_related(self, enum: int, i: int) -> str:
+    def add(self, path: str, class_name: str, name_image: str) -> None:
         """
-            данная функция предназначена для получения абсолютного или относительного пути
-            :i: - индекс который передается из функций get_data_path
-            :enum: - переменная которая нужно просто для конструкции if-else
-            :path: - возвращаемый путь ( абсолютный или относительный )
+            функция добавляет строку в файл аннотация
+            :path: - переменная содержащая путь ( например: "D:\Program Files\programmingLabs\Python\python-L-2-var-4\Lab2" )
+            :class_name: - имя класса ( под каталога ) например: CLASS_DEFAULT[0]
+            :name_image: - переменная которая содержит название картинки
         """
-        try:
-            path = ""
-            if enum == 0:
-                path = os.path.join(os.getcwd(), f'{self.dir_name}/{self.class_name}/{i:04d}.jpg')
-            elif enum == 1:
-                path = os.path.join(f'{self.dir_name}/{self.class_name}/{i:04d}.jpg')
-            elif enum == 2:
-                path = os.path.join(os.getcwd(), f'{self.dir_name}/{self.class_name}_{i:04d}.jpg')
-            elif enum == 3:
-                path = os.path.join(f'{self.dir_name}/{self.class_name}_{i:04d}.jpg')
-            elif enum == 4:
-                path = os.path.join(os.getcwd(), f'{self.dir_name}/{i:05d}.jpg')
-            elif enum == 5:
-                path = os.path.join(f'{self.dir_name}/{i:05d}.jpg')
-            return path
-        except OSError:
-            logging.warning("I'm sorry, but I couldn't find the path(")
-
-    def write_in_file_csv(self, data) -> None:
-        """
-            данная функция нужна для сохранения data ( с получ-ми  путями ) в файл .csv
-        """
-        try:
-            with open(f'{self.dir_name}/annotation.csv', 'a', newline='') as file:
-                wr = csv.writer(file)
-                wr.writerow(["absolute path", "relative path", "name"])
-                for i in range(999):
-                    wr.writerow([
-                        data["absolute path"][i],
-                        data["related path"][i],
-                        data["name"][i]
-                    ])
-                file.close()
-        except OSError:
-            logging.warning("error when writing the annotation file")
-
-    def get_data_path(self, data, enum) -> list:
-        """
-            данная функция нужна для формирования словаря data с путями
-            :enum: - переменная которая работает также как и в create_data_for_csv_file
-        """
-        try:
-            if enum == 0:
-                for i in range(1000):
-                    data["absolute path"].append(self.absolute_or_related(0, i + 1))
-                    data["related path"].append(self.absolute_or_related(1, i + 1))
-            elif enum == 1:
-                for i in range(1000):
-                    data["absolute path"].append(self.absolute_or_related(2, i + 1))
-                    data["related path"].append(self.absolute_or_related(3, i + 1))
-            elif enum == 2:
-                for i in range(len(self.list_numbers)):
-                    data["absolute path"].append(self.absolute_or_related(4, self.list_numbers[i]))
-                    data["related path"].append(self.absolute_or_related(5, self.list_numbers[i]))
-            data["name"] = self.dir_name
-            return data
-        except OSError:
-            logging.warning("error when generating a data dictionary with paths")
-
-    def create_data_for_csv_file(self, enum) -> None:
-        """
-            данная функция предназначенна для создания словаря data
-            :enum: - переменная которая необходима для оперд-я типа которая в дальнейшем необходима для понятия
-                     по какому if идти при формировании словаря data ( нужна только для if-else )
-
-        """
-        try:
-            data = {
-                "absolute path": [],
-                "related path": [],
-                "name": ""
-            }
-            if enum == 0:
-                data = self.get_data_path(data, 0)
-            elif enum == 1:
-                data = self.get_data_path(data, 1)
-            elif enum == 2:
-                data = self.get_data_path(data, 2)
-            else:
-                print("error invalid id!")
-            df = pd.DataFrame(data)
-            print(df)
-            self.write_in_file_csv(df)
-        except OSError:
-            logging.warning("error when creating a data dictionary")
-
-
-if __name__ == "__main__":
-    for element in range(len(CLASS_DEFAULT)):
-        obj = Data(CLASS_DEFAULT[element], "dataset")
-        obj.create_data_for_csv_file(0)
+        with open("annotation.csv", "a", encoding="utf-8", newline="") as file:
+            writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+            if self.number_lines == 0: # если кол-во строк = 0, то это заголовки файла аннотация
+                writer.writerow([
+                    "абсолютный путь",
+                    "относительный путь",
+                    "класс"
+                ])
+                self.number_lines += 1
+            writer.writerow([os.path.join(path, self.dir_name, class_name, name_image), os.path.join(self.dir_name, class_name, name_image), class_name])
+            self.number_lines += 1
