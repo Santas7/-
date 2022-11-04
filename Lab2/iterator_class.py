f5f58d4fcc3738fec4302601962f6d8875c18256
Santas7 <andreypanyshkin03@gmail.com>
2022-11-04 12:59:44 +0400

    изменил класс итератор

--- Lab2/iterator_class.py
+++ Lab2/iterator_class.py
@@ -1,33 +1,25 @@
 import main
 from main import Data
 import next_element as ne
-
+import os
 
 class IteratorOfExemplar:
     """
         класс итератор - чтобы обойти элементы внутри объекта вашего собственного класса
     """
 
-    def __init__(self, limit: int, obj: object, path: str):
-        self.limit = limit
+    def __init__(self, obj: type(Data), pointer: str):
         self.obj = obj
+        self.pointer = pointer
         self.counter = 0
-        self.this_path = path
-
-    def __iter__(self) -> None:
-        return self
 
     def __next__(self) -> str:
-        if self.counter < self.limit:
-            self.counter += 1
-            return ne.get_path_of_object_class(self.obj, self.this_path)
-        else:
-            raise StopIteration
-
+        """
+            возвращает следующий элемент
+        """
+        self.pointer = ne.next_element(self.obj, self.pointer)
+        return self.pointer
 
 if __name__ == "__main__":
-    num = int(input("Please input numbers iteration: "))
-    path = input("Please input path: ")  # например dataset/rose/0015.jpg
-    it = IteratorOfExemplar(num, Data(main.CLASS_DEFAULT[0], "dataset"), path)
-    for val in it:
-        print(val)
+    iter = IteratorOfExemplar(Data("dataset"),  "dataset\\rose\\0002.jpg")
+    print(iter.__next__())
