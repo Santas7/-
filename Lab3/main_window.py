cf91859228ad571058959911c0941442e92f398f
Santas7 <andreypanyshkin03@gmail.com>
2022-11-06 12:26:04 +0400

    добавил стартовые значения в конструкторе и подключил файлы из 2-й лабораторной работы + добавил реализацию методу create_annotation() - метод создания файла аннотации

--- Lab3/main_window.py
+++ Lab3/main_window.py
@@ -1,7 +1,14 @@
+import annotation
+import copy_elements as ce
+import random_of_copy as rc
+from annotation import Data
+from iterator_class import IteratorOfExemplar
 import sys
 from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QFileDialog, QVBoxLayout, QHBoxLayout, \
     QGridLayout
+from PyQt6.QtGui import QPixmap
+from PyQt6 import QtGui, QtWidgets
 
 
 class MainWindow(QMainWindow):
@@ -14,6 +21,12 @@ class MainWindow(QMainWindow):
         src = QLabel(f'Базовый датасет:\n{self.dataset_path}', self)
         layout = QGridLayout()
         layout.addWidget(src, 0, 0)
+
+        # стартовые значения
+        self.count_r = 1  # счетчик позиции
+        self.count_t = 1
+        self.s_p_rose = f"dataset\\{annotation.CLASS_DEFAULT[0]}\\0001.jpg"  # начальный для rose
+        self.s_p_tulip = f"dataset\\{annotation.CLASS_DEFAULT[1]}\\0001.jpg"  # начальный для tulip
 
         self.show()
 
@@ -51,6 +64,14 @@ class MainWindow(QMainWindow):
         """
             метод создания файла аннотации
         """
+        try:
+            ann = Data("dataset")
+            for i in range(1, 1000):
+                ann.add(self.dataset_path, f"{annotation.CLASS_DEFAULT[0]}", f"{i:04d}.jpg")
+            for i in range(1, 1000):
+                ann.add(self.dataset_path, f"{annotation.CLASS_DEFAULT[1]}", f"{i:04d}.jpg")
+        except OSError:
+            print("error")
 
     def copy_of_dataset(self):
         """
