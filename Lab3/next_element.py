ba7315c5ceecd1ea82717f9f46a3333852fae316
Santas7 <andreypanyshkin03@gmail.com>
2022-11-06 12:08:40 +0400

    добавил в next_element функцию back_element() - для возвращения предыдущего элемента

--- Lab3/next_element.py
+++ Lab3/next_element.py
@@ -2,6 +2,35 @@ import csv
 from annotation import Data
 
 
+def back_element(obj: type(Data), pointer: str) -> str:
+    """
+        данная функция получает объект и пойнт(точку или метку) и возращает следующий элемент
+    """
+    with open('annotation.csv', 'r', newline='') as file:
+        wr = csv.reader(file, delimiter=' ', quotechar='|')
+        i = 0  # счетчик
+        saver = 0
+        num_list = []
+        num = ''
+        for char in pointer:
+            if char.isdigit():
+                num = num + char
+            else:
+                if num != '':
+                    num_list.append(int(num))
+                    num = ''
+        if num != '':
+            num_list.append(int(num))
+        for row in wr:
+            if i != 0:
+                exist = pointer in row[1]
+                if exist:
+                    res = row[1].split(",")[1].replace("\\", "/")
+                    return res.replace(str(num_list[0]), str(num_list[0] - 1))
+            i += 1
+    return None
+
+
 def next_element(obj: type(Data), pointer: str) -> str:
     """
         данная функция получает объект и пойнт(точку или метку) и возращает следующий элемент
@@ -22,5 +51,5 @@ def next_element(obj: type(Data), pointer: str) -> str:
 
 if __name__ == "__main__":
     obj = Data("dataset")
-    next = next_element(obj, "dataset\\rose\\0751.jpg") # dataset\rose\0752.jpg
+    next = next_element(obj, "dataset\\rose\\0751.jpg")  # dataset\rose\0752.jpg
     print(next)
