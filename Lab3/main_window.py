ab335010454c5c439b10ec1dace9a692adf9ef8c
Santas7 <andreypanyshkin03@gmail.com>
2022-11-06 12:16:05 +0400

    создал класс MainWindow - основное окно приложения

--- Lab3/main_window.py
+++ Lab3/main_window.py
@@ -1,2 +1,15 @@
+import sys
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QFileDialog, QVBoxLayout, QHBoxLayout, QGridLayout
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QFileDialog, QVBoxLayout, QHBoxLayout, \
+    QGridLayout
+
+
+class MainWindow(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        self.show()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    app.exec()
