2dd9759b53b462b2378aeec8ab2bb7abfda3ad19
Santas7 <andreypanyshkin03@gmail.com>
2022-12-21 00:11:40 +0400

    добавил необходимые библиотеки + создал класс Cnn, Dataset ( без реализации )

---
+++ lab5/main.py
@@ -0,0 +1,32 @@
+# Import Library
+import random
+import glob
+from PIL import Image
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.model_selection import train_test_split
+from torchvision import datasets, models, transforms
+from torch.utils.data import DataLoader, Dataset
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+
+class Cnn(nn.Module):
+    pass
+
+
+class Dataset(torch.utils.data.Dataset):
+    """
+        this class is for loading our image sets
+    """
+
+
+
+def сreating_and_training_neural_network():
+
+
+if __name__ == '__main__':
+    сreating_and_training_neural_network()
